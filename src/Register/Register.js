6fca0beb43e6fa8a635c371fd1d464635ecff432
vitali <vitali.slepnyov@gmail.com>
2023-05-01 23:56:35 +0300

    refactor: удалить хук useFormValidator

--- src/Register/Register.js
+++ src/Register/Register.js
@@ -1,25 +1,21 @@
 import { Link, NavLink } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 
-import useFormValidator from '../../hooks/useFormValidator'; // Хук больше не используется
 import './Register.css';
 
 
 function Register({ onRegister }) {
 
-  const { isErrors, isValues, isValid, handleChangeInput } = useFormValidator();
-
   const { register, formState: { errors }, handleSubmit } = useForm();
 
   function submitForm(evt) {
-    if (isValid) {
-      evt.preventDefault();
-      onRegister({
-        name: isValues.name,
-        email: isValues.email,
-        password: isValues.password,
-      })
-    }
+    evt.preventDefault();
+    onRegister({
+      // name: name,
+      // email: email,
+      // password: password,
+    })
+
   }
 
   const onSubmit = (data) => {
@@ -41,13 +37,13 @@ function Register({ onRegister }) {
             className='register__input'
             type='text'
             name='name'
-            onChange={handleChangeInput}
+            // onChange={handleChangeInput}
             minLength={3}
             pattern='^[A-Za-zА-Яа-яЁё /s -]+$'
             required
           />
           <span className='register__error'>
-            {isErrors.name ? 'Недопустимое имя, минимальная длина 3 символа' : ''}
+            {/* {isErrors.name ? 'Недопустимое имя, минимальная длина 3 символа' : ''} */}
           </span>
 
           <p className='register__text-input'>E-mail</p>
@@ -56,28 +52,27 @@ function Register({ onRegister }) {
             className='register__input'
             type='email'
             name='email'
-            onChange={handleChangeInput}
+          // onChange={handleChangeInput}
           />
-          <span className='register__error'>{isErrors.email ? 'Недопустимый email' : ''}</span>
+          {/* <span className='register__error'>{isErrors.email ? 'Недопустимый email' : ''}</span> */}
 
           <p className='register__text-input'>Пароль</p>
           <input
             className='register__input'
             type='password'
             name='password'
-            onChange={handleChangeInput}
+            // onChange={handleChangeInput}
             minLength={5}
             required
           />
           <span className='register__error'>
-            {isErrors.password ? 'Недопустимый пароль, минимальная длина 5 символов' : ''}
+            {/* {isErrors.password ? 'Недопустимый пароль, минимальная длина 5 символов' : ''} */}
           </span>
         </fieldset>
 
         <button
           className='register__button'
           type='submit'
-          disabled={isValid ? false : true}
         >Зарегистрироваться</button>
         <p className='register__text'>
           Уже зарегистрированы?
