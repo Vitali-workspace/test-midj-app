3340243190843bb94ae50ad78b8df8d14250d727
vitali <vitali.slepnyov@gmail.com>
2023-05-01 23:59:02 +0300

    refactor: удалить хук useFormValidator в login

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,22 +1,17 @@
 import { Link, NavLink } from 'react-router-dom';
 
-import useFormValidator from '../../hooks/useFormValidator';
-
 import './Login.css';
 
 
 function Login({ onLogin }) {
 
-  const { isErrors, isValues, isValid, handleChangeInput } = useFormValidator();
-
   function submitForm(evt) {
-    if (isValid) {
-      evt.preventDefault();
-      onLogin({
-        'email': isValues.email,
-        'password': isValues.password,
-      });
-    }
+
+    evt.preventDefault();
+    onLogin({
+      // 'email': email,
+      // 'password': password,
+    });
   }
 
   return (
@@ -32,28 +27,28 @@ function Login({ onLogin }) {
             className='login__input'
             type='email'
             name='email'
-            value={isValues.email || ''}
-            onChange={handleChangeInput}
+            // value={isValues.email || ''}
+            // onChange={handleChangeInput}
             required
           />
-          <span className='login__error'>{isErrors.email ? 'Некорректный email' : ''}</span>
+          {/* <span className='login__error'>{isErrors.email ? 'Некорректный email' : ''}</span> */}
 
           <p className='login__text-input'>Пароль</p>
           <input
             className='login__input'
             type='password'
             name='password'
-            value={isValues.password || ''}
-            onChange={handleChangeInput}
+            // value={isValues.password || ''}
+            // onChange={handleChangeInput}
             required
             minLength={5}
           />
           <span className='login__error login__error_position'>
-            {isErrors.password ? 'Некорректный пароль' : ''}
+            {/* {isErrors.password ? 'Некорректный пароль' : ''} */}
           </span>
         </fieldset>
 
-        <button className='login__button' type='submit' disabled={isValid ? false : true}>Войти</button>
+        <button className='login__button' type='submit'>Войти</button>
         <p className='login__text'>
           Ещё не зарегистрированы?
           <Link className='login__link' to='/signup'> Регистрация</Link>
